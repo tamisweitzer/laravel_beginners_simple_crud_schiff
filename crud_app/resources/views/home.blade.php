521bdf4eb560df96902ccd291607190f25214712
Tami Sweitzer <tamara0715@gmail.com>
2024-06-22 14:55:47 -0500

    Start working on simple Laravel CRUD app.

---
+++ crud_app/resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Basic CRUD</title>
+  <link rel="stylesheet" href="./styles.css">
+</head>
+<body>
+  <h1>Simple CRUD in Laravel</h1>
+
+  <section class="section">
+    <h2>Register</h2>
+    <form action="/register" method="post">
+      @csrf;
+      <input type="text" name="name" placeholder="name">
+      <input type="text" name="email" placeholder="email">
+      <input type="text" name="password" placeholder="password">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+  </section>
+</body>
+</html>
---
+++ laravel_simple_crud.md
@@ -0,0 +1,23 @@
+# Laravel Tutorial For Beginners (Simple User CRUD App)
+by Brad Schiff
+
+ https://www.youtube.com/watch?v=cDEVWbz2PpQ&t=2067s
+
+--------------------------------------------------------
+
+This is a simple CRUD app to learn the basics of Laravel. We will cover routing and MVC architecture.
+
+
+## Create new project
+
+```sh
+composer create-project laravel/laravel .
+```
+
+## Start Dev Server
+
+```sh
+php artisan serve
+```
+
+Project is served on 127.0.0.1:8000 or 8001.
